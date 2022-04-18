24e2f4d34cf4ab2f2af13605bdd44726228ed4ed
estherwahnon <estherwa@hotmail.es>
2022-04-18 18:36:07 +0200

    ex6_2.3 ready for submitting

---
+++ ex6_2_3.py
@@ -0,0 +1,16 @@
+import time
+
+
+def timer(func,*args,**kwargs)->time:
+    """
+    A function that receives parameters and calculates the time that takes to finish running the function.
+    :param func:The function to calculate its time.
+    :param args:A pointer to the specific item.
+    :param kwargs:An argument list to the function.
+    :return:The time that takes for the function to finish running.
+    """
+    start_of_time = time.time()
+    func(*args,**kwargs)
+    end_of_time = time.time()
+    return end_of_time-start_of_time
+
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from ex6_2 import filter , is_positive, is_older
 from positive_numbers import get_the_positive_numbers
+from ex6_2_3 import timer
+
+
 
 if __name__ == '__main__':
 
     list_of_numbers = [-1,-2,-5,0, 1, 4, 2, 17, 35, 59, 99, 119]
-
 
     # Prints ex6.2
     print(tuple(filter(is_positive, list_of_numbers )))
@@ -12,6 +14,16 @@ if __name__ == '__main__':
     print(tuple(filter(is_older, list_of_numbers )))
     # Prints ex6.2.2
     print("The positive numbers in the list are: ",get_the_positive_numbers(list_of_numbers))
+    # Prints ex6.2.3
+    time= timer(print, "Hello")
+    print("First time",time)
+    time1=timer(zip, [0,1,2,3], [4, 5, 6])
+    print(time1)
+    time2=timer("Hi {name}".format, name="Bug")
+    print(time2)
+
+
+
 
 
 

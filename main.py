d05981ec27b4201f9699223177a93f6ded518cad
estherwahnon <estherwa@hotmail.es>
2022-04-18 17:50:02 +0200

    ex6_2.2 ready for submitting

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from ex6_2 import filter , is_positive, is_older
+from positive_numbers import get_the_positive_numbers
 
 if __name__ == '__main__':
 
-    the_ages = [-1,-2,-5,0, 1, 4, 2, 17, 35, 59, 99, 119]
+    list_of_numbers = [-1,-2,-5,0, 1, 4, 2, 17, 35, 59, 99, 119]
 
-    print(tuple(filter(is_positive, the_ages)))
 
-    print(tuple(filter(is_older, the_ages)))
+    # Prints ex6.2
+    print(tuple(filter(is_positive, list_of_numbers )))
+    # Prints ex6.2
+    print(tuple(filter(is_older, list_of_numbers )))
+    # Prints ex6.2.2
+    print("The positive numbers in the list are: ",get_the_positive_numbers(list_of_numbers))
+
+
+
 
 
 
